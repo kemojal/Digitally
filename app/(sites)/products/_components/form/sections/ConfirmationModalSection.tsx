bc29ecaf52d142e2c211258d100d90efc7fa0829
kemo <kemo3855@yahoo.com>
2024-05-25 05:21:22 +0800

    digitally add product

--- app/(sites)/products/_components/form/sections/ConfirmationModalSection.tsx
+++ app/(sites)/products/_components/form/sections/ConfirmationModalSection.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import DetailContainer from "../DetailContainer";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 const ConfirmationModalSection = () => {
   return (
@@ -8,6 +10,53 @@ const ConfirmationModalSection = () => {
         After purchase, customers will see a confirmation modal that includes a
         thank you message and a button to access your content.
       </p>
+      <div className="mt-4 space-y-4">
+        <div>
+          <label className="block text-gray-700">Title</label>
+          <Input
+            type="text"
+            placeholder="Thank you for your order"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          />
+          <small className="text-gray-500">
+            Customize the title in the purchase confirmation modal
+          </small>
+        </div>
+
+        <div>
+          <label className="block text-gray-700">Message</label>
+          <Textarea
+            placeholder="Woohoo! Your payment was successful, and your order is complete. A receipt is on its way to your inbox."
+            rows={4}
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          ></Textarea>
+          <small className="text-gray-500">
+            Customize the message in the purchase confirmation modal
+          </small>
+        </div>
+
+        <div>
+          <label className="block text-gray-700">Button text</label>
+          <Input
+            type="text"
+            placeholder="View Order"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          />
+          <small className="text-gray-500">
+            Customize the button text in the purchase confirmation modal. 36
+            chars max.
+          </small>
+        </div>
+
+        <div>
+          <label className="block text-gray-700">Button link</label>
+          <Input
+            type="text"
+            placeholder="digitally.com/orders"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          />
+        </div>
+      </div>
     </DetailContainer>
   );
 };
--- app/(sites)/products/_components/form/sections/EmailReceiptSection.tsx
+++ app/(sites)/products/_components/form/sections/EmailReceiptSection.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import DetailContainer from "../DetailContainer";
+import { Textarea } from "@/components/ui/textarea";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
 const EmailReceiptSection = () => {
   return (
@@ -8,6 +11,58 @@ const EmailReceiptSection = () => {
         After purchase, customers will receive an email receipt that includes a
         button to access your content.
       </p>
+
+      <div className="mt-4 space-y-4">
+        <div>
+          <label className="block text-gray-700">Thank you note</label>
+          <Textarea
+            placeholder=""
+            rows={4}
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          ></Textarea>
+          <small className="text-gray-500">
+            Add a personalized thank you note above the button in email receipts
+          </small>
+        </div>
+
+        {/* <div>
+          <label className="block text-gray-700">View Order</label>
+          <Input
+            type="text"
+            placeholder="Thank you for your order"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          />
+          <small className="text-gray-500">
+            Customize the button text in the email receipt. 36 chars max.
+          </small>
+        </div> */}
+
+        <div>
+          <label className="block text-gray-700">Button text</label>
+          <Input
+            type="text"
+            placeholder="View Order"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          />
+          <small className="text-gray-500">
+            Customize the button text in the purchase confirmation modal. 36
+            chars max.
+          </small>
+        </div>
+
+        <div>
+          <label className="block text-gray-700">Button link</label>
+          <Input
+            type="text"
+            placeholder="digitally.com/orders"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          />
+          <small className="text-gray-500">
+            Customize the button link in the email receipt{" "}
+            <Link href={""}>Help</Link>
+          </small>
+        </div>
+      </div>
     </DetailContainer>
   );
 };
